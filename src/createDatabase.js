ca92ede581ca1435cd0ae40ef2043a03126acf4c
Ankush Mudgil <101817825+mdglankush65@users.noreply.github.com>
2024-03-07 14:26:36 +0530

    deployment settings

--- src/createDatabase.js
+++ src/createDatabase.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 // Connect to DATABASE
-const DATABASE_URL = process.env.MONGODB_URI;
+const DATABASE_URL = process.env.MONGODB_URI || "mongodb+srv://mdglankush565:YXmIEIWMarHX4p8g@subscribers.kcsx7xr.mongodb.net/Users?retryWrites=true&w=majority&appName=Subscribers&tlsAllowInvalidCertificates=true";
 mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
